262ce77629cb935a10ec422f410cbce229e468e5
@iamsaphal <81309102+success-glich@users.noreply.github.com>
2023-04-24 20:19:57 +0545

    Add files via upload

---
+++ day2/main.js
@@ -0,0 +1,23 @@
+// variable is container that stores value.
+
+// var a = "hello world";
+
+// console.log(a);
+// // console.log(a);
+// var a = 12;
+// var b = 13;
+// console.log(a + b);
+//Let vs const vs var
+
+if (true) {
+  //   var name = "success";
+  //   const name = "success";
+  //   const a = 12;
+  //variableType variableName = value //strictly type
+  //   a = "success";
+  //   let a;
+  //   a = 12;
+  const a = 12;
+
+  console.log(a);
+}
