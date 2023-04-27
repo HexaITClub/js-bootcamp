c2210a6d1a731720bd37160b44b792a5532cc7b7
@iamsaphal <81309102+success-glich@users.noreply.github.com>
2023-04-27 19:51:56 +0545

    Day 3 File uploaded successfully

---
+++ Day03/script.js
@@ -0,0 +1,33 @@
+//If else Statement :
+
+// if (age > 16) {
+//   console.log("you are ready for driving");
+// }
+// if (age > 20) {
+//   console.log("ok your age is 20");
+// }
+//Switch case:
+// let name = "success";
+
+// switch (name) {
+//   case "safal":
+//     console.log("You are safal");
+//     break;
+//   case "success":
+//     console.log("You are success");
+//     break;
+//   default:
+//     console.log("default saphal");
+//     break;
+// }
+//
+
+let age = 22;
+
+let result = age > 18 ? "You allowed to drive" : "You are not allowed to drive";
+console.log(result);
+
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
+//Task : write a program to demonstrate While loop
